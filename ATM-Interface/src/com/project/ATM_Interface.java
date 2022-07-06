7b311656893a0ee2e64951342aab936a7f0edaa4
tharun <tharun@mail.com>
2022-07-06 10:17:47 +0530

    First Commit

---
+++ ATM-Interface/src/com/project/ATM_Interface.java
@@ -0,0 +1,80 @@
+package com.project;
+
+import java.util.Scanner;  
+
+public class ATM_Interface 
+{  
+    
+    public static void main(String args[] )  
+    {  
+        int balance = 100000, withdraw, deposit;  
+        
+        Scanner sc = new Scanner(System.in);  
+          
+        while(true)  
+        {  
+            System.out.println("ATM Machine\n");  
+            System.out.println("Choose 1 for Withdraw");  
+            System.out.println("Choose 2 for Deposit");  
+            System.out.println("Choose 3 for Check Balance");  
+            System.out.println("Choose 4 for EXIT\n");  
+            System.out.print("Choose the operation:");  
+              
+              
+            int choice = sc.nextInt();  
+            switch(choice)  
+            {  
+                case 1: System.out.print("Enter Amount to be withdrawn:");  
+                      
+       
+        withdraw = sc.nextInt();  
+                      
+         
+        if(balance >= withdraw)  
+        {  
+            
+            balance = balance - withdraw;  
+            System.out.println("Please collect your Amount");  
+        }  
+        else  
+        {  
+            System.out.println("Insufficient Balance");  
+        }  
+        System.out.println("");  
+        break;
+        
+        
+        
+        
+        
+   
+                case 2: System.out.print("Enter Amount to be deposite:");  
+                      
+       
+        deposit = sc.nextInt();  
+         
+        balance = balance + deposit;  
+        System.out.println("Your Amount has been depsited successfully");  
+        System.out.println("");  
+        break;  
+        
+        
+        
+        
+        
+   
+                case 3: System.out.println("Balance : "+balance);  
+        System.out.println("");  
+        break;  
+   
+        
+        
+        
+        
+                case 4: System.exit(0);  
+                
+                
+            }  
+        }  
+    }  
+} 
